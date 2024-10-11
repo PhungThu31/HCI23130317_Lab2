520e3bfa5c4ac2d30797b2678d587e918d57fa20
PhungThu31 <minhthu12575@gmail.com>
2024-10-11 22:22:45 +0700

    commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function validateLogin() {
-
-    window.location.href = 'dashboard.html'; 
-    return false;
-}
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    // Lưu thông tin xác thực bất kỳ (không kiểm tra tính hợp lệ)
+    localStorage.setItem("isAuthenticated", "true"); // Lưu thông tin xác thực
+    alert("Đăng nhập thành công!"); // Hiển thị thông báo đăng nhập thành công
+    window.location.href = "dashboard.html"; // Chuyển hướng đến trang dashboard
+    return false; // Ngăn không cho form được submit
+  }
+  
